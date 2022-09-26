0d54578b7ad95062651ebfbfd68ffbbb0246f86a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-09-26 10:50:47 +0200

    default data listener added

--- src/main/java/groupId/artifactId/storage/ProductStorage.java
+++ src/main/java/groupId/artifactId/storage/ProductStorage.java
@@ -10,13 +10,10 @@ import static groupId.artifactId.core.entity.ProductBuilder.create;
 public class ProductStorage implements IProductStorage {
     private static ProductStorage firstInstance = null;
     private final List<Product> productList = new ArrayList<>();
-    private int id =5;
+    private int id =1;
 
     private ProductStorage(){
-        this.productList.add(create().setId(1).setName("PC").setPrice(1500).setDiscount(200).setDescription("Nice and shiny").build());
-        this.productList.add(create().setId(2).setName("Laptop").setPrice(1200).setDiscount(100).setDescription("light weight").build());
-        this.productList.add(create().setId(3).setName("Headphones").setPrice(350).setDiscount(50).setDescription("Cool").build());
-        this.productList.add(create().setId(4).setName("Smartphone").setPrice(800).setDiscount(100).setDescription("New and fashionable").build());
+
     }
     public static ProductStorage getInstance() {
         synchronized (ProductStorage.class) {
